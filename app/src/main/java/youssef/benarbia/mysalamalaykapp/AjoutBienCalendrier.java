052dd19a327423bd27b7b61f6b6a9a78068433a3
YoussefBenarbia <youssef.benarbia@outlook.fr>
2021-03-19 08:57:13 +0100

    finalisation des interfaces graphiques d'ajout d'un bien.
    réglages des bugs

--- app/src/main/java/youssef/benarbia/mysalamalaykapp/AjoutBienCalendrier.java
+++ app/src/main/java/youssef/benarbia/mysalamalaykapp/AjoutBienCalendrier.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.timessquare.CalendarPickerView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class AjoutBienCalendrier extends AppCompatActivity {
     private Button saveButton;
@@ -14,11 +20,29 @@ public class AjoutBienCalendrier extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajout_bien_calendrier);
 
+        Date today = new Date();
+        Calendar nextYear = Calendar.getInstance();
+        nextYear.add(Calendar.YEAR, 1);
+        CalendarPickerView datePicker = findViewById(R.id.calendar);
+        datePicker.init(today, nextYear.getTime())
+                .inMode(CalendarPickerView.SelectionMode.RANGE)
+                .withSelectedDate(today);
 
-
-
-
-
+        datePicker.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(Date date) {
+                //String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(date);
+                Calendar calSelected = Calendar.getInstance();
+                calSelected.setTime(date);
+                String selectedDate = "" + calSelected.get(Calendar.DAY_OF_MONTH)
+                        + " " + (calSelected.get(Calendar.MONTH) + 1)
+                        + " " + calSelected.get(Calendar.YEAR);
+                Toast.makeText(AjoutBienCalendrier.this, selectedDate, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onDateUnselected(Date date) {
+            }
+        });
 
 
         this.saveButton = (Button) findViewById(R.id.saveButton);
